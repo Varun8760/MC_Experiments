b40c826529a3d1a50e3c9f355c770164074d6ef2
Varun8760 <varunpujari64@gmail.com>
2026-03-25 02:01:02 +0530

    Add files via upload

---
+++ EXP_07/src/SMSReceiver.java
@@ -0,0 +1,28 @@
+package com.example.createalertapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus != null) {
+                for (Object pdu : pdus) {
+                    SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
+                    String sender = smsMessage.getDisplayOriginatingAddress();
+                    String messageBody = smsMessage.getDisplayMessageBody();
+
+
+                    Toast.makeText(context, "SMS from: " + sender + "\n" + messageBody, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
